7346407783d023bf6263299aa2e87bcfdd999b39
Shruti <89388735+Shruti12072001@users.noreply.github.com>
2021-09-30 22:50:47 +0530

    Add files via upload

---
+++ shoppingwebsite/website.js
@@ -0,0 +1,44 @@
+
+
+
+$(document).ready(function(){
+   $('.food-slider').slick({
+      autoplay:true,
+      slidesToShow:3,
+      slidesToScroll:1,
+      prevArrow:".prev-btn",
+      nextArrow:".next-btn",
+      responsive:[
+         {
+            breakpoint:992,
+            settings:{
+             slidesToShow:2,
+            }
+         },
+         {
+          breakpoint:768,
+          settings:{
+           slidesToShow:1,
+          }
+       }
+      ]
+ 
+   });
+ 
+      $('.nav-trigger').click(function(){
+         $('.site-content-wrapper').toggleClass('scaled');
+      });
+
+});
+
+
+
+
+window.addEventListener('scroll' , function(){
+   if(window.pageXOffset>=151){
+      console.log("ok")
+   }
+});
+
+
+ 
